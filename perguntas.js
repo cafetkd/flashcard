7e10b5fd38804e9503a6713b8e6db723fb5433ad
cafetkd <suonogustavo@gmail.com>
2025-06-24 09:44:24 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,19 @@
+criaCartao(
+     'Programação',
+     'oque é javascript?',
+     'o javascript é uma linguagem de programação '
+
+)
+
+criaCartao(
+     'Geografia',
+     'Qual a capital do paraná?',
+     'a capital do paraná é Curitiba'
+)
+
+criaCartao(
+     'programação',
+     'o que é Python?',
+     'o Python é uma linguagem de programação '
+
+)
